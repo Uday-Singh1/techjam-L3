45bcd931b50e31c78fb93802bf0d1178784781e1
Uday-Singh G <91188742+Uday-Singh1@users.noreply.github.com>
2023-09-07 11:52:18 +0200

    first-commit

---
+++ Techjam-L3/getallen.php
@@ -0,0 +1,29 @@
+<?php
+  // Start een for loop  die van 33 tot 543 loopt
+        for ($i = 33; $i <= 543; $i++) {
+            $output = $i;
+
+            if ($i % 5 == 0 && $i % 9 == 0) {    //dit geeft aan dat je zegt dat als het te delen is door zowel 5 en 9 
+                $output = "Web";    //toont web als het te delen is door 5 en 9
+            } elseif ($i % 5 == 0) {    //anders deel je door 5
+                $output = "Software";   //toon software als het door 5 te delen is 
+            } elseif ($i % 9 == 0) {   //anders deel je door 9
+                $output = "Developer";   //toon software als het door 9 te delen is     
+            }
+
+            echo '<li>';    //toont een li 
+            if ($i % 2 == 0) {
+                echo '<strong>';    //strong is dikgedrukte lettertype
+            }
+
+             // Toon de waarde van $output
+            echo $output;
+
+            if ($i % 2 == 0) {
+                      // Als $i even is, sluit de sterke tag 
+                echo '</strong>';
+            }
+            //de list item word gesloten
+            echo '</li>';
+        }
+     ?>
